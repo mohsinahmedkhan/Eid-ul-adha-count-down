9a7746a361ca78529e1c72ecacd999b50a714b58
Mohsin Ahmed Khan <136707643+mohsinahmedkhan@users.noreply.github.com>
2024-06-29 18:15:19 +0500

    Add files via upload

--- Eid-ul-adha-count-down/app.js
+++ Eid-ul-adha-count-down/app.js
@@ -1,22 +1,27 @@
-setInterval (function() {
-var date = new Date() ;
-var eid = new Date("17 june 2024") ;
-var diff = eid - date;
+setInterval(function () {
+    var date = new Date().getTime()
+    var eid = new Date("17 june 2024 00:00:00").getTime()
 
-let eidDayLeft = diff / (1000 * 60 * 60 * 24)
-let eidHoursLeft = diff / (1000 * 60 * 60) % 24
-let eidMinLeft = diff / (1000 * 60) % 60
-let eidSecLeft = diff / (1000) % 60
+    var diff = eid - date
 
-let resultDay = `${Math.floor(eidDayLeft)}`
-let resultHr = `${Math.floor(eidHoursLeft)}`
-let resultMin = `${Math.floor(eidMinLeft)}`
-let resultSec = `${Math.floor(eidSecLeft)}`
+    var eidDayLeft = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var eidHoursLeft = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var eidMinLeft = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var eidSecLeft = Math.floor((diff % (1000 * 60)) / 1000);
 
-document.getElementById("box1").innerText = resultDay
-document.getElementById("box2").innerText = resultHr
-document.getElementById("box3").innerText = resultMin
-document.getElementById("box4").innerText = resultSec
+    document.getElementById("box1").innerText = eidDayLeft
+    document.getElementById("box2").innerText = eidHoursLeft
+    document.getElementById("box3").innerText = eidMinLeft
+    document.getElementById("box4").innerText = eidSecLeft
 
-})
-setInterval()
+    if (diff < 0) {
+        clearInterval()
+        document.getElementById("box1").innerText = "00";
+        document.getElementById("box2").innerText = "00";
+        document.getElementById("box3").innerText = "00";
+        document.getElementById("box4").innerText = "00";
+    }
+
+}, 1000)
+
+
